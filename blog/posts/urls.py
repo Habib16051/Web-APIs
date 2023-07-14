df112f5575f2e70815ae4a8f8dbf52cd9d694660
Habib16051 <habibmhr143@gmail.com>
2023-07-14 07:12:49 +0600

    checking new file

---
+++ blog/posts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import PostList, PostDetail
+
+urlpatterns = [
+    path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
+    path("", PostList.as_view(), name="post_list"),
+]
