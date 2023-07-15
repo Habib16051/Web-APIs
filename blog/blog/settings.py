89b843df2a9239641c2ddda1f84245559c943459
Habib16051 <habibmhr143@gmail.com>
2023-07-15 13:15:57 +0600

    added environs for the future action

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+from environs import Env  # new
+env = Env()  # new
+env.read_env()  # new
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
